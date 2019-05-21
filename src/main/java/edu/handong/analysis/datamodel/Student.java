ede50e51c1c5e6a55181fa01a257aa53ec3c9dff
SilverJun <jangej1031@naver.com>
2019-05-21 23:33:05 +0900

    Update Student

--- src/main/java/edu/handong/analysis/datamodel/Student.java
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -23,15 +23,23 @@ public class Student {
 	
 	public HashMap<String,Integer> getSemestersByYearAndSemester()
 	{
+		int i = 1;
+		
 		for (Course course : coursesTaken)
 		{
-			if (semestersByYearAndSemester.containsKey(Integer.toString(course.getYearTaken())))
+			String tempString = new String();
+			tempString += Integer.toString(course.getYearTaken());
+			tempString += "-";
+			tempString += Integer.toString(course.getSemesterCourseTaken());
+			
+			if (semestersByYearAndSemester.containsKey(tempString))
 			{
 				continue;
 			}
 			else
 			{
-				semestersByYearAndSemester.put(Integer.toString(course.getYearTaken()), course.getSemesterCourseTaken());
+				System.out.println(tempString);
+				semestersByYearAndSemester.put(tempString, i++);
 			}
 		}
 		
@@ -42,9 +50,27 @@ public class Student {
 	{
 		int count = 0;
 		
+		String yearSemester = new String();
+		
+		for (String key : semestersByYearAndSemester.keySet()) {				// 키 스트링을 찾는다.
+            if (semestersByYearAndSemester.get(key).intValue() == semester) {
+            	yearSemester = key;
+            	break;
+            }
+        }
+		
 		for (Course course : coursesTaken)
-			if (course.getSemesterCourseTaken() == semester)
+		{
+			String tempString = new String();
+			tempString += Integer.toString(course.getYearTaken());
+			tempString += "-";
+			tempString += Integer.toString(course.getSemesterCourseTaken());
+			
+			if (tempString.equals(yearSemester))		// 해시맵을 만들때 처럼 문자열을 만들고 키랑 비교한다.
+			{
 				count++;
+			}
+		}
 		
 		return count;
 	}
